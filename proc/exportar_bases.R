9535993e5b03f3b014666f1a4345109de75a7042
gabcortes97 <gabriel.cortes@uchile.cl>
2025-07-02 17:56:16 -0400

    exportacion bases

--- proc/exportar_bases.R
+++ proc/exportar_bases.R
@@ -3,6 +3,13 @@ p_load(tidyverse)
 
 load(file="input/data/cohesion_merge.RData")
 
+otros <- datos %>% select(orgullonacional,
+                          igualdad_ingresos_lapop,
+                          trabajo_duro,
+                          percepcion_corrupcion,
+                          interes_politica,
+                          sub_prosocial)
+
 datos <- datos %>% select(it1, aoj11, vic1ext,
                            confianza_congreso,
                            confianza_part_politicos_lapop,
@@ -18,109 +25,19 @@ datos <- datos %>% select(it1, aoj11, vic1ext,
                            cohesion_vertical,
                            cohesion_general)
 
-# Calidad subdimensiones
-
-calidad_year <- datos %>%
-  as.data.frame %>%
-  select(-c(pais, wave)) %>%
-  mutate(across(everything(), ~ !is.na(.))) %>%
-  as.data.frame() %>%
-  mutate(
-    sub_confianza   = ifelse(rowSums(cbind(it1)) > 0, FALSE, TRUE),
-    sub_seguridad  = ifelse(rowSums(cbind(aoj11, vic1ext)) > 1, FALSE, TRUE),
-    sub_confianza_inst  = ifelse(rowSums(cbind(confianza_congreso,
-                                                        confianza_part_politicos_lapop,
-                                                        confianza_poder_judicial)) > 1, FALSE, TRUE),
-    sub_democracia = ifelse(rowSums(cbind(apoyo_democracia, satisfaccion_democracia)) > 1, FALSE, TRUE),
-    justicia_distribucion = ifelse(rowSums(cbind(justicia_distribucion)) > 0, FALSE, TRUE),
-  ) %>%
-  select(c("sub_confianza",
-           "sub_seguridad",
-           "sub_confianza_inst",
-           "sub_democracia",
-           "justicia_distribucion")) %>%
-  mutate(
-    pais = datos$pais,
-    wave = datos$wave
-  ) %>%
-  select(pais,wave,everything())
-
-
-for(i in 1:nrow(datos)){
-  # print(i)
-  for(x in names(calidad_year)[3:7]){
-    if(calidad_year[i,x]){
-      datos[i,x] <- NA
-    }
-  }
-}
-
-# Calidad dimensiones
-
-calidad_year <- datos %>%
-  as.data.frame %>%
-  select(-c(pais, wave)) %>%
-  mutate(across(everything(), ~ !is.na(.))) %>%
-  as.data.frame() %>%
-  mutate(
-    cohesion_horizontal   = ifelse(rowSums(cbind(sub_confianza, sub_seguridad)) > 1, FALSE, TRUE),
-    cohesion_vertical = ifelse(rowSums(cbind(sub_confianza_inst, sub_democracia, justicia_distribucion)) > 1, FALSE, TRUE)) %>%
-  select(c(cohesion_horizontal,
-           cohesion_vertical)) %>%
-  mutate(
-    pais = datos$pais,
-    wave = datos$wave
-  ) %>%
-  select(pais,wave,everything())
-
-
-for(i in 1:nrow(datos)){
-  # print(i)
-  for(x in names(calidad_year)[3:4]){
-    if(calidad_year[i,x]){
-      datos[i,x] <- NA
-    }
-  }
-}
-
-# Calidad general
-
-calidad_year <- datos %>%
-  as.data.frame %>%
-  select(-c(pais, wave)) %>%
-  mutate(across(everything(), ~ !is.na(.))) %>%
-  as.data.frame() %>%
-  mutate(
-    cohesion_general   = ifelse(rowSums(cbind(cohesion_vertical,
-                                              cohesion_horizontal)) > 1, FALSE, TRUE)) %>%
-  select(c(cohesion_general)) %>%
-  mutate(
-    pais = datos$pais,
-    wave = datos$wave
-  ) %>%
-  select(pais,wave,everything())
-
-columnas_objetivo <- c("cohesion_general")  # Agrega más si lo deseas
-for(i in 1:nrow(datos)) {
-  if (isTRUE(calidad_year$cohesion_general[i])) {
-    datos[i, columnas_objetivo] <- NA
-  }
-}
-
 datos <- datos %>% filter(wave!=2020 & wave!=2021)
 
 # Exportar
 
-load(file = "input/data/country_vars.rdata")
+load(file="input/data/variables_macro.RData")
 
-country_vars <- country_vars %>%
-  rename("wave" = "date")
+country_vars <- var_macro
 
-df <- merge(datos,
-            country_vars,
-            by = c("pais","wave"),
-            all.x = TRUE) %>%
-  select(-id,-country)
+
+# df <- merge(datos,
+#             otros,
+#             by = c("pais","wave"),
+#             all.x = TRUE)
 
 
 
@@ -128,6 +45,35 @@ df <- merge(datos,
 #   mutate(pais = droplevels(pais)) %>%
 #   as_tibble()%>%
 #   complete(wave,pais)
+df <- datos %>%
+  select(Ola = wave,
+         País = pais,
+         "Confianza Interpersonal"= sub_confianza,
+         "Seguridad Pública"= sub_seguridad,
+         "Confianza en las Instituciones"= sub_confianza_inst,
+         "Actitudes a la Democracia"= sub_democracia,
+         "Justicia Redistributiva"= justicia_distribucion,
+         "Cohesión horizontal" = cohesion_horizontal,
+         "Cohesión vertical" = cohesion_vertical,
+         "Cohesión general"= cohesion_general,
+         # "Orgullo Nacional"= orgullonacional,
+         # "Importancia Igualdad de Ingresos"= igualdad_ingresos_lapop,
+         # "Importancia Trabajo Duro"= trabajo_duro,
+         # "Percepción de Corrupción"= percepcion_corrupcion,
+         # "Interés en la Política"= interes_politica,
+         # "Comportamiento Prosocial"= sub_prosocial
+         ) %>% 
+  pivot_longer(cols = 3:10,names_to = "Variable",values_to = "Valor", values_drop_na = FALSE)
+
+save(df,file="input/data/base_shiny_minima.rdata")
+
+# Otros indicadores
+
+df <- merge(datos,
+            otros,
+            by = c("pais","wave"),
+            all.x = TRUE)
+
 df <- df %>%
   select(Ola = wave,
          País = pais,
@@ -139,17 +85,83 @@ df <- df %>%
          "Cohesión horizontal" = cohesion_horizontal,
          "Cohesión vertical" = cohesion_vertical,
          "Cohesión general"= cohesion_general,
-         "Coef. Gini" = gini,
-         "PIB per capita (miles)" = gdp
+         "Orgullo Nacional"= orgullonacional,
+         "Importancia Igualdad de Ingresos"= igualdad_ingresos_lapop,
+         "Importancia Trabajo Duro"= trabajo_duro,
+         "Percepción de Corrupción"= percepcion_corrupcion,
+         "Interés en la Política"= interes_politica,
+         "Comportamiento Prosocial"= sub_prosocial
   ) %>% 
-  pivot_longer(cols = 3:12,names_to = "Variable",values_to = "Valor", values_drop_na = FALSE)
+  pivot_longer(cols = 3:16,names_to = "Variable",values_to = "Valor", values_drop_na = FALSE)
 
-df <- df %>% rbind(df %>%
-                     group_by(Ola, Variable) %>%
-                     summarise(Valor = mean(Valor, na.rm = TRUE)) %>%
-                     mutate(País = "Promedio") %>%
+save(df,file="input/data/base_shiny_otros.rdata")
+
+ ### Procesar base macro
+library(stringi)
+library(dplyr)
+
+paises_estandar <- c(
+  "Argentina", "Bahamas", "Belize", "Bolivia", "Brazil",
+  "Canada", "Chile", "Colombia", "Costa Rica", "Dominican Republic",
+  "Ecuador", "El Salvador", "Grenada", "Guatemala", "Guyana",
+  "Haiti", "Honduras", "Jamaica", "Mexico", "Nicaragua",
+  "Panama", "Paraguay", "Peru", "Puerto Rico", "Suriname",
+  "Trinidad & Tobago", "United States", "Uruguay", "Venezuela"
+)
+
+
+# Función para normalizar texto
+normalizar <- function(x) {
+  stri_trans_general(x, "Latin-ASCII") |> tolower()
+}
+
+# Ahora hacemos la clasificación
+country_vars <- country_vars %>%
+  mutate(pais_normalizado = normalizar(nombre_pais),
+         País = sapply(pais_normalizado, function(x) {
+           match <- NA
+           for (p in paises_estandar) {
+             if (grepl(normalizar(p), x, ignore.case = TRUE)) {
+               match <- p
+               break
+             }
+           }
+           match
+         })) 
+
+unique(country_vars$País)
+
+country_vars <- country_vars %>%
+  mutate(País= case_when(id_pais=="TTO" ~ "Trinidad & Tobago",
+                         id_pais=="USA" ~ "United States",
+                         id_pais=="DOM" ~ "Dominican Republic",
+                         id_pais=="BLZ" ~ "Belize",
+                         id_pais=="GRD" ~ "Grenada",
+                         id_pais=="BRA" ~ "Brazil",
+                         .default= País)) %>%
+  select("Ola"= año,
+         País,
+         "Variable"= indicador,
+         "Valor"= valor)
+
+unique(country_vars$País)
+
+df <- rbind(df, country_vars)
+
+
+##################
+
+
+df <- df %>%
+  rbind(df %>%
+          group_by(Ola, Variable) %>%
+          summarise(
+            Valor = if (all(is.na(Valor))) NA_real_ else mean(Valor, na.rm = TRUE),
+            .groups = "drop"
+          ) %>%
+          mutate(País = "Promedio")) %>%
                      ungroup() %>%
-                     select(Ola, País, Variable, Valor)) %>%
+                     select(Ola, País, Variable, Valor) %>%
   filter(Ola!=2021)
 
 save(df,file = "input/data/base_shiny.rdata")
@@ -157,7 +169,14 @@ save(df,file = "input/data/base_shiny.rdata")
 # Ola, País, Variable, Valor (incluye NA) # 2000 filas.
 
 
+### Datos para el maapa
+
+
 load(file = "input/data/world_data.rdata")
+load(file="input/data/country_vars.RData")
+
+country_vars <- country_vars %>%
+  rename("wave" = "date")
 
 df <- merge(datos,
             country_vars,
